cefcdaaacdb0b3a66956c9ab883389cda0e8e918
Vladyslav Nahornyi <vladnagornyj2@gmail.com>
2023-06-20 15:19:34 +0100

    Solved

---
+++ MyListEnum.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1_module3
+{
+    public class MyListEnum<T> : IEnumerator
+    {
+        private T[] _items;
+        private int _position = -1;
+
+        public MyListEnum(T[] items)
+        {
+            _items = items;
+        }
+
+        public T Current
+        {
+            get
+            {
+                if (_position == -1 || _position >= _items.Length)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                return _items[_position];
+            }
+        }
+
+        object IEnumerator.Current => Current;
+
+        public bool MoveNext()
+        {
+            if (_position < _items.Length - 1)
+            {
+                _position++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            _position = -1;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ Starter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1_module3
+{
+    public static class Starter
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Welcom to the BMW Motor Show");
+            Console.WriteLine();
+            string[] cars = { "M3", "M4", "M5", "M8", "X5M", "X3M", "X7M", "X8M" };
+            var bmwmotorshow = new MyList<string>();
+            bmwmotorshow.AddRange(cars);
+            Console.WriteLine();
+            Console.WriteLine("add M2:");
+            bmwmotorshow.Add("new M2 Competition");
+            bmwmotorshow.Sort();
+            foreach (var car in bmwmotorshow)
+            {
+                if (car != default)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("DElete M5:");
+            bmwmotorshow.Remove("M5");
+            foreach (var car in bmwmotorshow)
+            {
+                if (car != default)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("delete by index-2:");
+            bmwmotorshow.RemoveAt(2);
+            foreach (var car in bmwmotorshow)
+            {
+                if (car != default)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
